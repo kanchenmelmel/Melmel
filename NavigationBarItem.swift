4d1f1686d210271fd0243642037d609189d02d13
Patrick Gao <patrickgao0922@gmail.com>
2016-05-25 14:44:09 +1000

    NavigationBar

---
+++ NavigationBarItem.swift
@@ -0,0 +1,13 @@
+//
+//  NavigationBarItem.swift
+//  Melmel
+//
+//  Created by Work on 25/05/2016.
+//  Copyright © 2016 Melmel. All rights reserved.
+//
+
+import UIKit
+
+class NavigationBarItem: UIBarItem {
+    
+}
