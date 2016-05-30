58d85d221b4d40308edeffc23cbcbd2ec69c2a2d
Patrick Gao <patrickgao0922@gmail.com>
2016-05-30 23:37:21 +1000

    StartReachability
    Created ReachabilityManager.swift under Utilities

---
+++ ReachabilityManager.swift
@@ -0,0 +1,15 @@
+//
+//  ReachabilityManager.swift
+//  Melmel
+//
+//  Created by Work on 30/05/2016.
+//  Copyright © 2016 Melmel. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration
+
+class ReachabilityManager {
+    static var reachabilityManager = ReachabilityManager()
+    init(){}
+}
