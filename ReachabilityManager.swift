f83f9bcbf7acc8ef261fba6dde54086a2bdef4fa
Patrick Gao <patrickgao0922@gmail.com>
2016-05-31 17:01:38 +1000

    BasicReachabilityFunctionality
    The basic reachability functionality added for
    MelGuideTableViewController.swift using ashleymills/Reachability.swift
    https://github.com/ashleymills/Reachability.swift

--- ReachabilityManager.swift
+++ ReachabilityManager.swift
@@ -9,7 +9,55 @@
 import Foundation
 import SystemConfiguration
 
+
+
 class ReachabilityManager {
-    static var reachabilityManager = ReachabilityManager()
-    init(){}
+    static var sharedReachabilityManager = ReachabilityManager()
+    
+    
+    
+    var reachability:Reachability?
+    let reachabilityChangedNotification = "ReachabilityChangedNotification"
+    
+    init(){
+        do {
+            reachability = try Reachability.reachabilityForInternetConnection()
+        } catch {
+            return
+        }
+        
+       // NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(self.reachabilityChanged(_:)), name: reachabilityChangedNotification, object: reachability)
+        
+    }
+    
+    func isReachable() -> Bool {
+        return (reachability?.isReachable())!
+    }
+    
+    func isReachableViaWiFi() -> Bool {
+        return (reachability?.isReachableViaWiFi())!
+    }
+    
+    func isReachableViaWWAN() -> Bool {
+        return (reachability?.isReachableViaWWAN())!
+    }
+    
+    
+    
+    
+//    @objc func reachabilityChanged(note:NSNotification){
+//        let reachability = note.object as! Reachability
+//        
+//        if reachability.isReachable(){
+//            if reachability.isReachableViaWiFi(){
+//                networkStatus = .isReachableViaWiFi
+//            }else {
+//                networkStatus = .isReachableViaWWAN
+//            }
+//            
+//        } else {
+//            networkStatus = .notReachable
+//        }
+//    }
+    
 }
