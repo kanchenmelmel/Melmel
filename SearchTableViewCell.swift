a403c250010b8688c24c2eca3ed779625e60112a
KaiP-598 <kaipengwu598@live.cn>
2016-07-16 17:53:03 +1000

    UpdateSearchUI

--- SearchTableViewCell.swift
+++ SearchTableViewCell.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class SearchTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    
     @IBOutlet weak var featureImage: UIImageView!
+    
     @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var titleLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
