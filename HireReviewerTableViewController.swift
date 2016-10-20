92a96dc098023e0668382a797e2d75d4c0aa903c
Patrick Gao <patrickgao0922@gmail.com>
2016-10-20 13:44:50 +1100

    AddHireReviewer

--- HireReviewerTableViewController.swift
+++ HireReviewerTableViewController.swift
@@ -1,15 +1,15 @@
 //
-//  HireEvaluatorViewController.swift
+//  HireReviewerTableViewController.swift
 //  Melmel
 //
-//  Created by Wenyu Zhao on 3/10/16.
+//  Created by Work on 20/10/16.
 //  Copyright © 2016 Melmel. All rights reserved.
 //
 
 import UIKit
 import MelMobile
 
-class HireEvaluatorViewController: UIViewController {
+class HireReviewerTableViewController: ContactFormTableViewController {
 
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
@@ -62,7 +62,7 @@ class HireEvaluatorViewController: UIViewController {
         
         let email = Email(
             from: "Melmel iOS <hire-evaluator-ios.noreply@melmel.com.au>",
-            to: "Melmel Consulting <wenyuzhaox@gmail.com>",
+            to: "Melmel Consulting <\(ReceiverEmailAddress)>",
             title: "Melmel测评员申请 (Melmel iOS客户端<招募测评员>)",
             content: "姓名：\(name)\n邮箱：\(_email)\n手机：\(mobile)\n微信：\(wechat)\n\n自我介绍：\n\n\(content)"
         )
@@ -71,5 +71,63 @@ class HireEvaluatorViewController: UIViewController {
             self.showDialog("success")
         }
     }
+    
+    // MARK: - Table view data source
+
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
