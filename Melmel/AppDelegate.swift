01ed0107ecaa46c29b543f31b7872417a218e941
Patrick Gao <patrickgao0922@gmail.com>
2016-10-31 16:05:36 +1100

    FinalCommit

--- Melmel/AppDelegate.swift
+++ Melmel/AppDelegate.swift
@@ -243,7 +243,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDe
                 let webVC = storyboard.instantiateViewController(withIdentifier: "webVC") as! DiscountWebViewController
                 webVC.webRequestURLString = userInfo["url"] as? String
                 webVC.discountTitle = userInfo["title"] as? String
-                webVC.navigationItem.setRightBarButton(nil, animated: true)
+//                webVC.navigationItem.setRightBarButton(nil, animated: true)
                 webVC.navigationItem.title = "墨尔本优惠"
                 NavRootVC.navigationController?.pushViewController(webVC, animated: true)
             }
